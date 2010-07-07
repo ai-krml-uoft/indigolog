b7c030833389eb46fea1c21562157b220b157e2f
Sebastian Sardina <ssardina@gmail.com>
2010-07-07 10:47:06 +0000

    - Fixed loading of vanilla interpreter for MACOSX (Yves)

--- current/Interpreters/indigolog-vanilla_swi.pl
+++ current/Interpreters/indigolog-vanilla_swi.pl
@@ -40,7 +40,7 @@
 :- style_check(-discontiguous).	% SWI dependent!
 :-multifile prim_action/1, causes_val/4, poss/2, proc/2, prim_fluent/1.
 
-:- consult(indigolog-vanilla).
+:- consult('indigolog-vanilla').
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % EOF: indigolog-vanilla_swi.pl
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
