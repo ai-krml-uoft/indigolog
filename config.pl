c5fcf6685a14f77a73e63a8f21a944466c1ca694
ssardina <ssardina@gmail.com>
2024-05-12 22:49:39 +1000

    factor out config file to root folder

---
+++ config.pl
@@ -0,0 +1,105 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FILE: lib/systemvar.pl
+%
+%    WRITTEN BY: Sebastian Sardina (ssardina@cs.toronto.edu)
+%    Time-stamp: <2008-11-29 09:11:03 ssardina>
+%    TESTED    : ECLiPSe 5.4 on RedHat Linux 6.2-7.2
+%    TYPE CODE : system independent predicates
+%
+% DESCRIPTION: wide-system variables and constants
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%                             July 9, 2002
+%
+% This software was developed by the Cognitive Robotics Group under the
+% direction of Hector Levesque and Ray Reiter.
+%
+%        Do not distribute without permission.
+%        Include this notice in any copy made.
+%
+%
+%         Copyright (c) 2000 by The University of Toronto,
+%                        Toronto, Ontario, Canada.
+%
+%                          All Rights Reserved
+%
+% Permission to use, copy, and modify, this software and its
+% documentation for non-commercial research purpose is hereby granted
+% without fee, provided that the above copyright notice appears in all
+% copies and that both the copyright notice and this permission notice
+% appear in supporting documentation, and that the name of The University
+% of Toronto not be used in advertising or publicity pertaining to
+% distribution of the software without specific, written prior
+% permission.  The University of Toronto makes no representations about
+% the suitability of this software for any purpose.  It is provided "as
+% is" without express or implied warranty.
+% THE UNIVERSITY OF TORONTO DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+% SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+% FITNESS, IN NO EVENT SHALL THE UNIVERSITY OF TORONTO BE LIABLE FOR ANY
+% SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+% RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+% CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+% CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% The following definition of constants are provided:
+%
+% -- main_dir(Dir) : main directory of the whole code
+% -- type_prolog(T) : current prolog engine is T (swi/ecl/sics/van)
+% -- executable_path(A, P) : P is the executable path for software A
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% AUTOMATIC LOAD OF REQUIRED LIBRARIES %%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This subsection does the following:
+%  (a) provides main_dir(Path)
+%  (b) loads the neccessary compatibility library: compat_ecl or compat_swi
+%  (c) sets ` to be the string construct (using set_backquoted_string)
+
+
+:- prolog_load_context(directory, Dir), assert(root_indigolog(Dir)).
+
+% Path is the root path of the IndiGolog system
+% In SWI Pwd will be a string already
+main_dir(Path):- getenv('PATH_INDIGOLOG',Pwd),
+                 (string(Pwd) -> atom_string(APwd, Pwd) ; APwd=Pwd),
+                 concat_atom([APwd, '/'], Path).
+
+
+% This is the initialization needed for each type of Prolog used
+:- 	root_indigolog(Dir),
+   	directory_file_path(Dir, 'lib', LibDir),
+   	assert(library_directory(LibDir)),
+	use_module(library(eclipse_swi)), init_eclipse_lib, % ECLIPSE Compat lib
+	use_module(library(tools_swi)),
+	use_module(library(time)),	% for call_with_time_limit/2
+	style_check(-discontiguous),
+	set_prolog_flag(optimise, true),
+	set_backquoted_string.
+
+
+interpreter(indigolog_plain, F) :-
+	root_indigolog(Dir),
+	directory_file_path(Dir, "interpreters/indigolog_plain_swi.pl", F).
+
+
+% Defines the path of executables used to define device managers
+executable_path(swi, '/usr/bin/swipl').
+executable_path(eclipse, '/opt/bin/eclipse-pl').  % if available
+executable_path(tcltk, '/usr/bin/wish').
+executable_path(xterm, '/usr/bin/xterm').
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% EOF: lib/systemvar.pl
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
