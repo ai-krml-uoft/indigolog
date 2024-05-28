7c14db97311a47e8359ae1579c5ff44a1b415b11
ssardina <ssardina@gmail.com>
2024-05-29 09:52:00 +1000

    tcltk has end_indi built in

--- env/exog.tcl
+++ env/exog.tcl
@@ -1,4 +1,4 @@
-set id 0
+set id 1
 label .l_exog  -text "Exogenous Event:"
 label .l_hist  -text "Past events:"
 entry .entry_exog -width 30 -relief sunken -textvariable cmd
@@ -11,16 +11,20 @@ pack .b_exit -padx 1m -pady 1m
 pack .l_hist -padx 1m -pady 1m
 
 
-bind .entry_exog <Return> {
-# check if we have more tha 10 buttons, delete older buttons if so
-set id [expr $id + 1]
-if {$id > 10} {
-destroy .b[expr $id - 10]
-}
-button .b$id -command "puts stdout $cmd." -text $cmd
+button .b$id -command "puts stdout end_indi." -text end_indi
 pack .b$id -fill x
-.b$id invoke
-.entry_exog delete 0 end
+# .b$id invoke
+
+bind .entry_exog <Return> {
+    # check if we have more tha 10 buttons, delete older buttons if so
+    set id [expr $id + 1]
+    if {$id > 9} {
+    destroy .b[expr $id - 10]
+    }
+    button .b$id -command "puts stdout $cmd." -text $cmd
+    pack .b$id -fill x
+    .b$id invoke
+    .entry_exog delete 0 end
 }
 
 
