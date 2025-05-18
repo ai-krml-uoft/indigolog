d8b499db2d9a3560f783da2373d8935226afb46c
ssardina <ssardina@gmail.com>
2025-05-18 18:18:28 +1000

    add trans_start with number of transitions

--- interpreters/transfinal.pl
+++ interpreters/transfinal.pl
@@ -36,20 +36,24 @@
 % 	final(E3, H3).
 
 do(E, H) :- do(E, [], H).
+
+% do(_, [A|_], _) :- exog_action(A), writeln(A), fail.
+% do(_, [A|H], _) :-
+% 	A = withdrawal_by_applicant(_),
+% 	writeln([A|H]),
+% 	fail.
+
 do(E, H, H) :- final(E, H).
 do(E, H, H2) :- trans(E, H, E1, H1), do(E1, H1, H2).
+
 
 
 % Transitive clousure of trans/4
 trans_star(E, H, E, H).
 trans_star(E, H, E1, H1) :-
 	trans(E, H, E2, H2),
-	(var(H1) ->
-		true 			% always succ if var(H1)
-	;
-		% If H1 is given, check H2 is a subhistory of H1
-		once(before(H2, H1))
-	),
+	% before/1: from evaluator; checks H2 is subhistory of H1
+	(var(H1) -> true ; once(before(H2, H1))),
     trans_star(E2, H2, E1, H1).
 
 % transitive version of final/2 combined
@@ -57,11 +61,12 @@ final_star(E, H) :- final(E, H).
 final_star(E, H) :- trans_star(E, H, E2, H), E2 \= E, final_star(E2, H).
 
 % trans/5 performs a defined number N of consecutive trans steps
-trans(E, H, E, H, 0) :- !.
-trans(E, H, E1, H1, N) :-
+trans_star(E, H, E, H, 0) :- !.
+trans_star(E, H, EN, HN, N) :-
+	trans(E, H, E1, H1),
+	(var(H1) -> true ; once(before(H1, HN))),
 	N2 is N-1,
-	trans(E, H, E2, H2, N2),
-	trans(E2, H2, E1, H1).
+	trans_star(E1, H1, EN, HN, N2).
 
 
 % Stores a node/4 entry in DB with Id and program E and history H
@@ -70,7 +75,7 @@ store_node(Id, E, H) :-
 	assert(node(Id, N2, E, H)),
 	assert(counter(N2)).
 
-% Evolve (E, H) as much as possible until (E2, H2)
+% Evolve (E, H) as much as possible (i.e., until it blocks)
 trans_block(E, H, E2, H2) :- trans_star(E, H, E2, H2), \+ trans(E2, H2, _, _).
 
 
